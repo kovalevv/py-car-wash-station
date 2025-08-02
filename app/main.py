7758cda70d551a6067af35396e94752fd2cea547
kovalevv <vl.kovalevskyi@gmail.com>
2025-08-02 23:29:04 +0200

    Fix on initial commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Car:
     def __init__(
             self,
@@ -33,7 +34,7 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(total_income, 1)
 
-    def calculate_washing_price(self, car: Car):
+    def calculate_washing_price(self, car: Car) -> float:
         return round(
             car.comfort_class * (self.clean_power - car.clean_mark)
             * self.average_rating / self.distance_from_city_center, 1
@@ -50,4 +51,3 @@ class CarWashStation:
                 total + new_rating
             ) / self.count_of_ratings, 1
         )
-
